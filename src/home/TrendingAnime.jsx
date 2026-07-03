375808bd45e8830b2806f3557761573651d55822
ayoubhanine <ayoubhanine2000@gmail.com>
2026-07-03 11:28:36 +0100

    fixer des details concernant responsive design

--- src/home/TrendingAnime.jsx
+++ src/home/TrendingAnime.jsx
@@ -70,7 +70,7 @@ export default function TrendingAnime() {
       </div>
 
       
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
         {trendingAnime.map((anime) => (
           <AnimeCard
             key={anime.mal_id}
